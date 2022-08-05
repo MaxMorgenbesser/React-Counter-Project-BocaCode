6ebb86b04d265667ea9671a536e8992760f6082a
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-05 13:31:13 -0400

    Counter works and foes everyhing it is supposed to, needs css

---
+++ src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import React from 'react'
+
+
+export default function Counter (){
+    const [count,setCount] = useState(0)
+   const minusOne = ()=> {setCount(count-1)}
+    const addOne = () => {setCount(count+1)}
+    const reset = () => {setCount(0)}
+    const Checkcount = () =>{
+        if (count>0){
+            return minusOne()
+        }
+        else{
+            return reset
+        }
+    }
+
+
+    return (<>
+    <h3>Count: {count}</h3>
+    <button onClick={Checkcount}>-</button>
+    <button onClick={reset}>Reset</button>
+    <button onClick={addOne}>+</button>
+
+    </>
+    )
+}
